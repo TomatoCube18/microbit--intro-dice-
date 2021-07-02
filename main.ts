127c44779a0560e8d1469e2a7e5c5061793499ff
Tomato Cube <47109215+TomatoCube18@users.noreply.github.com>
2021-07-03 04:02:08 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,56 @@
+let diceValue = 0
+input.onGesture(Gesture.Shake, function () {
+    diceValue = Math.randomRange(1, 6)
+    if (diceValue == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (diceValue == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            . . . . .
+            . . . # .
+            . . . . .
+            `)
+    } else if (diceValue == 3) {
+        basic.showLeds(`
+            . . # . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . # . .
+            `)
+    } else if (diceValue == 4) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+    } else if (diceValue == 5) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+    } else {
+        basic.showLeds(`
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            `)
+    }
+})
+basic.forever(function () {
+	
+})
